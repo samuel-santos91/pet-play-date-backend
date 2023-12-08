c875805f9a0b0e26ee7b580b1f11cef6f07dc827
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-12-08 20:04:19 +1100

    adds jwt auth filter to security config, authentication provider and manager to app config

--- src/main/java/com/sam/backend/config/AppConfig.java
+++ src/main/java/com/sam/backend/config/AppConfig.java
@@ -1,13 +1,22 @@
 package com.sam.backend.config;
 
+import com.sam.backend.user.CustomUserDetailsService;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
+
+  @Autowired
+  private CustomUserDetailsService userDetailsService;
 
   @Bean
   public Dotenv dotenv() {
@@ -17,5 +26,21 @@ public class AppConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    // DAoAuthProv needs UserDetailsService with loadUserByUsername method
+    provider.setUserDetailsService(userDetailsService);
+    provider.setPasswordEncoder(this.passwordEncoder());
+    return provider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+    AuthenticationConfiguration config
+  ) throws Exception {
+    return config.getAuthenticationManager();
   }
 }
--- src/main/java/com/sam/backend/config/SecurityConfig.java
+++ src/main/java/com/sam/backend/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package com.sam.backend.config;
 
+import com.sam.backend.jwt.JwtAuthFilter;
 import jakarta.servlet.DispatcherType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+  @Autowired
+  private JwtAuthFilter jwtAuthFilter;
 
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
@@ -26,6 +32,10 @@ public class SecurityConfig {
           .permitAll()
           .anyRequest()
           .authenticated()
+      )
+      .addFilterBefore(
+        jwtAuthFilter,
+        UsernamePasswordAuthenticationFilter.class
       )
       .sessionManagement(session ->
         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
