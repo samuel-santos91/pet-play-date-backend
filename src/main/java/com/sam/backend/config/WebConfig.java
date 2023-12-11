f40fa87f588ef58dcdafb253b443b9025ebaae44
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-12-11 15:39:28 +1100

    adds cors configuration

---
+++ src/main/java/com/sam/backend/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.sam.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  public void addCorsMappings(CorsRegistry registry) {
+    String[] allowedOrigins = {
+      "http://localhost:3000/",
+      "http://127.0.0.1:3000/",
+    };
+
+    registry
+      .addMapping("/**")
+      .allowedOrigins(allowedOrigins)
+      .allowedMethods("GET", "POST", "DELETE", "PATCH")
+      .allowedHeaders("*");
+  }
+}
