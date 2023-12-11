6d72c422eab4d38e7d4b4d074d8b1eb46579b6d3
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-12-11 16:15:12 +1100

    updates origin url cors

--- src/main/java/com/sam/backend/config/WebConfig.java
+++ src/main/java/com/sam/backend/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
   public void addCorsMappings(CorsRegistry registry) {
     String[] allowedOrigins = {
-      "http://localhost:3000/",
-      "http://127.0.0.1:3000/",
+      "http://localhost:5173/",
+      "http://127.0.0.1:5173/",
     };
 
     registry
